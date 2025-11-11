d5d88b32a3a1c164075534109a013bdf778907b6
DOAN NGUYEN HA NAM <24521100@gm.uit.edu.vn>
2025-11-12 01:03:52 +0700

    Add files via upload

---
+++ lab3_TH/bai02/bai02/Mainboard.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace bai02
+{
+    partial class Mainboard
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_listen = new Button();
+            listViewCommand = new ListView();
+            SuspendLayout();
+            // 
+            // btn_listen
+            // 
+            btn_listen.Location = new Point(794, 116);
+            btn_listen.Name = "btn_listen";
+            btn_listen.Size = new Size(232, 46);
+            btn_listen.TabIndex = 0;
+            btn_listen.Text = "Listen";
+            btn_listen.UseVisualStyleBackColor = true;
+            btn_listen.Click += btn_listen_Click;
+            // 
+            // listViewCommand
+            // 
+            listViewCommand.Location = new Point(141, 227);
+            listViewCommand.Name = "listViewCommand";
+            listViewCommand.Size = new Size(885, 310);
+            listViewCommand.TabIndex = 1;
+            listViewCommand.UseCompatibleStateImageBehavior = false;
+            listViewCommand.SelectedIndexChanged += listViewCommand_SelectedIndexChanged;
+            listViewCommand.View = View.List;
+            // 
+            // Mainboard
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1189, 616);
+            Controls.Add(listViewCommand);
+            Controls.Add(btn_listen);
+            Font = new Font("Segoe UI", 15F);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Mainboard";
+            Text = "Mainboard";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ColumnHeader colMessage;
+        private Button btn_listen;
+        private ListView listViewCommand;
+    }
+}
---
+++ lab3_TH/bai02/bai02/Mainboard.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace bai02
+{
+    public partial class Mainboard : Form
+    {
+        public Mainboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_listen_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.IsBackground = true;
+            serverThread.Start();
+        }
+
+        void StartUnsafeThread()
+        {
+            int bytesReceived = 0;
+            byte[] recv = new byte[1];
+
+            Socket clientSocket;
+
+            Socket listenerSocket = new Socket(
+                 AddressFamily.InterNetwork,
+                 SocketType.Stream,
+                 ProtocolType.Tcp
+             );
+
+            IPEndPoint ipepServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            listenerSocket.Bind(ipepServer);
+
+            listenerSocket.Listen(10);
+
+            clientSocket = listenerSocket.Accept();
+
+            listViewCommand.Items.Add(new ListViewItem("New client connected"));
+
+            while (clientSocket.Connected)
+            {
+                string text = "";
+                do
+                {
+                    bytesReceived = clientSocket.Receive(recv);
+                    if (bytesReceived <= 0) break;
+
+                    text += Encoding.ASCII.GetString(recv, 0, bytesReceived);
+                }
+                while (text.Length == 0 || (text[^1] != '\n' && text[^1] != '\r'));
+
+                text = text.TrimEnd('\r', '\n');
+                if (text.Length > 0)
+                    listViewCommand.Items.Add(new ListViewItem(text));
+            }
+
+            listenerSocket.Close();
+        }
+
+        private void listViewCommand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
